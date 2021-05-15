c56bd7bd65b5e1997cdbe270c34aeea9df8aa19f
NanaShirakashi <haribo.0730@gmail.com>
2021-05-15 16:09:51 +0900

    change to main function

--- Single_Tello_Test/tello_test.py
+++ Single_Tello_Test/tello_test.py
@@ -4,31 +4,36 @@ from datetime import datetime
 import time
 import os
 
-start_time = str(datetime.now())
+def main():
+    df_now = datetime.now()
+    start_time = df_now.strftime('%Y-%m-%d %H:%M:%S')
 
-file_name = sys.argv[1]
+    file_name = sys.argv[1]
 
-f = open(file_name, "r")
-commands = f.readlines()
+    f = open(file_name, "r")
+    commands = f.readlines()
 
-tello = Tello()
-for command in commands:
-    if command != '' and command != '\n':
-        command = command.rstrip()
+    tello = Tello()
+    for command in commands:
+        if command != '' and command != '\n':
+            command = command.rstrip()
 
-        if command.find('delay') != -1:
-            sec = float(command.partition('delay')[2])
-            print('delay %s' % sec)
-            time.sleep(sec)
-            pass
-        else:
-            tello.send_command(command)
+            if command.find('delay') != -1:
+                sec = float(command.partition('delay')[2])
+                print('delay %s' % sec)
+                time.sleep(sec)
+                pass
+            else:
+                tello.send_command(command)
 
-log = tello.get_log()
-dir_name = './log'
-os.makedirs(dir_name,exist_ok=True)
-out = open(dir_name + '/' + start_time + '.txt', 'w')
-for stat in log:
-    stat.print_stats()
-    str = stat.return_stats()
-    out.write(str)
+    log = tello.get_log()
+    dir_name = './log'
+    os.makedirs(dir_name,exist_ok=True)
+    out = open(dir_name + '/' + start_time + '.txt', 'w')
+    for stat in log:
+        stat.print_stats()
+        str = stat.return_stats()
+        out.write(str)
+
+if __name__ == "__main__":
+    main()
